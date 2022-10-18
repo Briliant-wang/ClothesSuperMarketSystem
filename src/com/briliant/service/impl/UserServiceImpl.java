00f5092f714f10f8dec00dd4a44ebc18925e4e5f
Briliant-Wang <70946467+Briliant-wang@users.noreply.github.com>
2022-10-18 21:04:17 +0800

    V1.0
    add remarks

--- src/com/briliant/service/impl/UserServiceImpl.java
+++ src/com/briliant/service/impl/UserServiceImpl.java
@@ -7,6 +7,12 @@ import com.briliant.utils.EmptyUtils;
 import com.briliant.utils.UserIO;
 
 public class UserServiceImpl implements UserService {
+    /**
+     * 实现注册用户的功能
+     * @param user
+     * @return 返回一个注册成功的用户
+     * @throws BusinessException
+     */
     @Override
     public User register(User user) throws BusinessException {
         UserIO userIO = new UserIO();
@@ -15,6 +21,13 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
+    /**
+     * 实现登录账号功能
+     * @param username 用户名
+     * @param password 密码
+     * @return 用户的存在与否
+     * @throws BusinessException
+     */
     @Override
     public User login(String username,String password) throws BusinessException {
         if (EmptyUtils.isEmpty(username)) {
